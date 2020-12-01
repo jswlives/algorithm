768e50c3df1a38836ab27d1a4a368c6e13758d4d
jiangshaowei <jiangshaowei@gamesci.com.cn>
2020-12-01 20:31:47 +0800

    ci

--- alth/main.cpp
+++ alth/main.cpp
@@ -53,6 +53,35 @@ bool TestSort(int iArraySize)
 	return true;
 }
 
+
+
+int HashKeyToIdx(const void* pvKey, int iKeyLen)
+{
+	std::cout << pvKey << std::endl;
+	unsigned int seed = 131; // 31 131 1313 13131 131313 etc..
+	unsigned int hash = 0;
+
+	unsigned char* pTmp = (unsigned char*)pvKey;
+
+	for (int i = 0; i < iKeyLen; i++)
+	{
+		uint8_t ucT = *pTmp;
+		uint16_t a = ucT;
+		std::cout << a << "|";
+		hash = hash * seed + ucT;
+		pTmp++;
+	}
+	std::cout << std::endl;
+
+	return ((hash & 0x7FFFFFFF));
+}
+
+int id_2_hash(uint64_t ullRoleID)
+{
+	return HashKeyToIdx(&ullRoleID, (int)sizeof(ullRoleID));
+}
+
+
 int main()
 
 {
@@ -78,6 +107,9 @@ int main()
 	arrays.add(4);
 	arrays.add(5);
 
-	std::cout << binsearch_first_ge(arrays, 3) << std::endl;
+	//std::cout << binsearch_first_ge(arrays, 3) << std::endl;
+
+	std::cout << id_2_hash(1111) << std::endl;
+	std::cout << id_2_hash(1111) << std::endl;
 
 }
